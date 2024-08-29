4490454973246efa04ac9f8a77b3d16d78fdf541
Poosh <luigifan359@gmail.com>
2024-08-29 13:20:12 -0400

    Finir

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,15 @@ package org.example;
 //Okay it turns out there's other things that are actually what I needed to do.
 public class Main {
     public static void main(String[] args) {
+        //Creating 2 vehicles to make sure they work
         Vehicle v1 = new Vehicle();
         Vehicle v2 = new Vehicle(2, "Jet Black", 4.0f, "Soylent");
 
+        //Creating the 2 cars, one without and one with arguments
         Car c1 = new Car();
         Car c2 = new Car(4, "Red", 80.0f, "Gasoline", "Wumpus Incorporated");
 
+        //Testing honk and display on both cars.
         c1.honk();
         c1.displayInfo();
 
@@ -25,6 +28,7 @@ class Vehicle {
     float engineSize;
     String fuelType;
 
+    //Creates a new Vehicle with default attributes
     public Vehicle() {
         numberOfWheels = 4;
         color = "Blue";
@@ -32,6 +36,7 @@ class Vehicle {
         fuelType = "Magic";
     }
 
+    //Creates a new Vehicle with provided attributes
     public Vehicle(int wheels, String hue, float size, String fuel) {
         numberOfWheels = wheels;
         color = hue;
@@ -39,6 +44,7 @@ class Vehicle {
         fuelType = fuel;
     }
 
+    //Setters and Getters for each variable in the Vehicle class
     public void setNumberOfWheels(int num){
         numberOfWheels = num;
     }
@@ -75,16 +81,28 @@ class Vehicle {
 class Car extends Vehicle{
     String brand;
 
+    //Creates a new car by referencing it's super and providing a default value.
     public Car(){
         super();
         brand = "Wumpus Incorporated";
     }
 
+    //Creates a new car by referencing it's super, using provided values.
     public Car(int num, String hue, float size, String type, String producer){
         super(num, hue, size, type);
         brand = producer;
     }
 
+    //Set and Get for the Car specific value, Brand
+    public void setBrand(String producer){
+        brand = producer;
+    }
+
+    public String getBrand(){
+        return brand;
+    }
+
+    //Print all values in the Car class.
     public void displayInfo(){
         System.out.println(brand);
         System.out.println(numberOfWheels);
@@ -93,6 +111,7 @@ class Car extends Vehicle{
         System.out.println(fuelType);
     }
 
+    //Honk.
     public void honk(){
         System.out.println("Honk Honk!");
     }
